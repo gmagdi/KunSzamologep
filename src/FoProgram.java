19b15921c568cce4d92d43289dd92710707ea452
GeorgeCooney <georgecooney@users.noreply.github.com>
2017-11-20 21:41:13 +0100

    Masodik commit
    Add metódus hozzáadva és a kód kicsit megtisztítva.

--- src/FoProgram.java
+++ src/FoProgram.java
@@ -4,15 +4,9 @@ import java.util.stream.IntStream;
 
 public class FoProgram {
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("Szia!");
-        Scanner sc;
-        sc = new Scanner(System.in);
-        String szam;
+    public static int add(String numbers){
         int osszeg = 0;
-        szam = sc.nextLine();
-        sc.close();
-        String[] szamokSzovegkent = szam.split(",");
+        String[] szamokSzovegkent = numbers.split(",");
         int[] szamok = new int[szamokSzovegkent.length];
         for (int y = 0; y < szamokSzovegkent.length; y++) {
             szamok[y] = Integer.parseInt(szamokSzovegkent[y]);
@@ -22,6 +16,23 @@ public class FoProgram {
             osszeg += szamok[i];
             i++;
         }
-        System.out.print(osszeg);
+        return osszeg;
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.println("Szia!");
+        Scanner sc;
+        sc = new Scanner(System.in);
+        String szam;
+        szam = sc.nextLine();
+        sc.close();
+        if(!szam.isEmpty()){
+            System.out.println(add(szam));
+        }
+        else
+        {
+            System.out.println("0");
+        }
+
+
     }
 }
