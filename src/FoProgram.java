246233b94d2e1b59f7a7305da4845e32971f5ce1
GeorgeCooney <georgecooney@users.noreply.github.com>
2017-11-21 15:58:41 +0100

    Vegleges
    Minden funkciója működik(elvileg).

--- src/FoProgram.java
+++ src/FoProgram.java
@@ -4,22 +4,35 @@ import java.util.stream.IntStream;
 
 public class FoProgram {
 
-    public static int add(String numbers){
+    public static String add(String numbers){
         int osszeg = 0;
-        String[] szamokSzovegkent = numbers.split("[-+/%,~*_;:&]");
+        String hiba ="";
+        String[] szamokSzovegkent = numbers.split("[+/%,~*_;:&]");
         int[] szamok = new int[szamokSzovegkent.length];
         for (int y = 0; y < szamokSzovegkent.length; y++) {
             szamok[y] = Integer.parseInt(szamokSzovegkent[y]);
         }
         int i = 0;
         while (i < szamok.length) {
+            if(szamok[i]>=0){
             osszeg += szamok[i];
+            }
+            else
+            {
+                hiba+=" "+szamok[i];
+            }
             i++;
         }
-        return osszeg;
+        String vissza;
+        if(hiba!="")
+            vissza ="Negatives not allowed:"+hiba;
+        else{
+            vissza = ""+osszeg;
+        }
+        return vissza;
     }
     public static void main(String[] args) throws Exception {
-        System.out.println("Kérlek add meg az összeadandó számokat az alábbi karakterek egyikével elválasztva! -+/%,~*_;:&");
+        System.out.println("Kérlek add meg az összeadandó számokat az alábbi karakterek egyikével elválasztva! +/%,~*_;:&");
         Scanner sc;
         sc = new Scanner(System.in);
         String szam;
